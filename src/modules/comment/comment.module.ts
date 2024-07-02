9198907394ad30f8125d6dd9c102f0c014572c8d
heifengli001 <13675665+heifengli001@users.noreply.github.com>
2024-07-02 16:22:16 +0800

    🐞 fix(业务模块): 评论模块未引入Repository导致DataSource为undefined问题

--- src/modules/comment/comment.module.ts
+++ src/modules/comment/comment.module.ts
@@ -4,10 +4,15 @@ import { CommentController } from './comment.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommentEntity } from './comment.entity';
 import { ArticleModule } from '../article/article.module';
+import { BaseDefaultRepository } from '@/core/repository/base.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CommentEntity]), ArticleModule],
+  imports: [
+    TypeOrmModule.forFeature([CommentEntity, BaseDefaultRepository]),
+    ArticleModule,
+  ],
   controllers: [CommentController],
   providers: [CommentService],
+  exports: [CommentService],
 })
 export class CommentModule {}
